9227b23507c051b71b8e228134d335bb5c44caf7
No <No@no.no>
2020-02-03 00:20:44 +0500

    Initial

---
+++ src/com/company/Main.java
@@ -0,0 +1,177 @@
+package com.company;
+
+import org.w3c.dom.ls.LSOutput;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import javax.swing.plaf.FileChooserUI;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.*;
+import java.net.Socket;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.LinkOption;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+import java.util.TreeSet;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        try {
+            for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (Exception e) {
+            // If Nimbus is not available, you can set the GUI to another look and feel.
+        }
+
+
+        TaskGiverWindow tgw=new TaskGiverWindow();
+
+        tgw.jb.addActionListener(new ActionListener()
+        {
+            public void actionPerformed(ActionEvent e)
+            {
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            int w = 595 * 4;
+                            int h = 832 * 4;
+                            tgw.printLine("Генерация работы...");
+                            tgw.printLine("    *Формат страницы: A4");
+                            tgw.printLine("    *Размер одного изображения: "+w+"x"+h+" пикселей");
+                            tgw.printLine("    *Формат файла: PNG");
+                            //Scanner s = new Scanner(System.in);
+                            //tgw.printLine("Имя ученика");
+                            String username = tgw.name.getText();
+                            tgw.printLine("    *Папка с работой: "+username);
+                            File directory = new File("" + username);
+                            if (! directory.exists()) {
+                                tgw.printLine("Создание папки...");
+                                //tgw.printLine("ОК!");
+                                directory.mkdir();
+                            }else{
+                                tgw.printLine("    *Найдена старая работа с таким же названием");
+                                tgw.printLine("Удаление старых данных...");
+                                deleteDirectoryRecursion(directory.toPath());
+                                //tgw.printLine("ОК!");
+                                tgw.printLine("Создание папки...");
+                                directory.mkdir();
+                                //tgw.printLine("ОК!");
+                            }
+                            String type = tgw.folder.getText();
+                            tgw.printLine("    *Папка с заданиями: "+type);
+                            int amount = (int) tgw.amount.getValue();
+                            tgw.printLine("    *Количество задач: "+amount);
+                            File dir = new File(type);
+                            File[] filesArray = dir.listFiles();
+                            ArrayList<File> files = new ArrayList<>();
+                            for (int i = 0; i < filesArray.length; i++) {
+                                files.add(filesArray[i]);
+                            }
+                            if (amount > files.size()) {
+                                tgw.printLine("    !Это больше, чем доступное количество задач. Укажите новое значение и прпробуйте снова.");
+                                tgw.printLine("    *В папке найдено задач: "+filesArray.length);
+                                return;
+                            }
+                            tgw.printLine("    *В папке найдено задач: "+filesArray.length);
+                            tgw.printLine("    *Для создания работы будет использован следующий код: "+type.hashCode());
+                            Random r = new Random(type.hashCode());
+                            TreeSet<Integer> ts = new TreeSet<>();
+
+                            tgw.printLine("Формирование работы...");
+                            while (ts.size() < amount) {
+                                int tr=r.nextInt(files.size());
+                                ts.add(tr);
+                            }
+                            //tgw.printLine("ОК!");
+                            int curpos = 0;
+                            BufferedImage bi = new BufferedImage(w, h, BufferedImage.TYPE_INT_RGB);
+                            Graphics2D g2d = (Graphics2D) bi.getGraphics();
+                            g2d.setPaint(new Color(255, 255, 255));
+                            g2d.fillRect(0, 0, w, h);
+                            String result = username + System.lineSeparator();
+                            tgw.printLine("Начало записи...");
+                            int iter = 1;
+                            int taskNumber=1;
+                            for (int t : ts) {
+                                tgw.printLine("Добавление задания "+files.get(t).getName()+"...");
+                                BufferedImage task = ImageIO.read(files.get(t));
+                                double k = (double) (w) / (double) (task.getWidth());
+                                task = bwresize(task, (int) (task.getWidth() * k), (int) (task.getHeight() * k));
+                                result += taskNumber+". "+files.get(t).getName() + System.lineSeparator();
+                                if (curpos + task.getHeight() > bi.getHeight()) {
+                                    tgw.printLine("Сохранение " + iter + " страницы ...");
+                                    ImageIO.write(bi, "png", new File("" + username + "/" + username + ", лист №" + iter + ".png"));
+                                    bi = new BufferedImage(w, h, BufferedImage.TYPE_INT_RGB);
+                                    g2d = (Graphics2D) bi.getGraphics();
+                                    g2d.setPaint(new Color(255, 255, 255));
+                                    g2d.fillRect(0, 0, w, h);
+                                    curpos = 0;
+                                    iter++;
+                                    //tgw.printLine("ОК!");
+                                }
+                                g2d.drawImage(task, 0, curpos, null);
+                                curpos += task.getHeight();
+                                taskNumber++;
+                                //tgw.printLine("ОК!");
+                            }
+                            //tgw.printLine("ОК!");
+                            tgw.printLine("Сохранение "+iter+" страницы ...");
+                            ImageIO.write(bi, "png", new File("" + username + "/" + username + ", лист №" + iter + ".png"));
+                            //tgw.printLine("ОК!");
+                            tgw.printLine("Сохранение файла с описанием задания...");
+                            tgw.printLine("    *Название файла описания: "+ username + ".txt");
+                            BufferedWriter bw = new BufferedWriter(new FileWriter("" + username + "/" + username + ".txt"));
+                            bw.write(result);
+                            bw.close();
+                            tgw.printLine("Отправка файла с ответами на сервак Альберта... (У кого-то будет пять)");
+                            Socket sock=new Socket("https://math-task-checker.herokuapp.com/addVariant/",80);
+                            bw = new BufferedWriter( new OutputStreamWriter(sock.getOutputStream()));
+                            bw.write(result);
+                            bw.close();
+                            //tgw.printLine("ОК!");
+                            tgw.printLine("ОК!");
+                        } catch (Exception ex) {
+                            tgw.printLine("     !Произошла ошибка. Убедитесь, что правильно ввели названия папок и попробуйте снова.");
+                            ex.printStackTrace();
+                            tgw.printLine("Ошибка!");
+                        }
+                    }
+                }).start();
+            }
+        });
+    }
+
+    public static BufferedImage bwresize(BufferedImage img, int newW, int newH) {
+        Image tmp = img.getScaledInstance(newW, newH, Image.SCALE_SMOOTH);
+        BufferedImage dimg = new BufferedImage(newW, newH, BufferedImage.TYPE_INT_ARGB);
+
+        Graphics2D g2d = dimg.createGraphics();
+        g2d.drawImage(tmp, 0, 0, null);
+        g2d.dispose();
+
+        return dimg;
+    }
+    static void deleteDirectoryRecursion(Path path) throws IOException {
+        if (Files.isDirectory(path, LinkOption.NOFOLLOW_LINKS)) {
+            try (DirectoryStream<Path> entries = Files.newDirectoryStream(path)) {
+                for (Path entry : entries) {
+                    deleteDirectoryRecursion(entry);
+                }
+            }
+        }
+        Files.delete(path);
+    }
+}
---
+++ src/com/company/TaskGiverWindow.java
@@ -0,0 +1,88 @@
+package com.company;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.Random;
+
+public class TaskGiverWindow extends JFrame {
+    Random r=new Random();
+    JSpinner amount=new JSpinner();
+    JTextField name=new JTextField();
+    JTextField folder=new JTextField();
+    JPanel tadd=new JPanel();
+    JPanel tadd3=new JPanel();
+    JPanel tadd2=new JPanel();
+    JButton jb=new JButton("Создать работу!");
+    JTextArea jtext =new JTextArea();
+    //JScrollPane jscroll =new JScrollPane();
+    public TaskGiverWindow(){
+        super("Создание задания");
+        amount.setValue(10);
+        jtext.setLineWrap(true);
+        jtext.setOpaque(false);
+        jtext.setBackground(new Color(255,255,255,0));
+        jtext.setEnabled(false);
+        jtext.setDisabledTextColor(new Color(0,0,0));
+        //jscroll.getViewport().setOpaque(false);
+        //jscroll.setOpaque(false);
+        jtext.setEditable(false);
+        jtext.setColumns(30);
+        tadd2.setLayout(new BorderLayout());
+        tadd.setLayout(new BoxLayout(tadd,BoxLayout.PAGE_AXIS));
+        tadd.add(Box.createVerticalStrut(10));
+        JLabel label=new JLabel("Параметры работы     ");
+        Font f = label.getFont();
+        label.setFont(f.deriveFont(f.getStyle() | Font.BOLD));
+        tadd.add(label);
+        tadd.add(Box.createVerticalStrut(30));
+        tadd.add(new JLabel("Название работы или имя ученика:     "));
+        tadd.add(name);
+        tadd.add(Box.createVerticalStrut(30));
+        tadd.add(new JLabel("Папка с заданиями:     "));
+        tadd.add(folder);
+        tadd.add(Box.createVerticalStrut(30));
+        tadd.add(new JLabel("Количество заданий:     "));
+        tadd.add(amount);
+        tadd.add(Box.createVerticalStrut(30));
+        tadd.add(jb);
+        tadd.add(Box.createVerticalStrut(10));
+        tadd2.add(tadd, BorderLayout.CENTER);
+        tadd2.add(Box.createHorizontalStrut(20),BorderLayout.WEST);
+        tadd2.add(Box.createHorizontalStrut(20),BorderLayout.EAST);
+        tadd2.add(Box.createVerticalStrut(20),BorderLayout.SOUTH);
+        tadd2.add(Box.createVerticalStrut(20),BorderLayout.NORTH);
+        //jscroll.add(jtext);
+
+        JLabel labell=new JLabel("Вывод программы:");
+        Font fl = labell.getFont();
+        labell.setFont(fl.deriveFont(fl.getStyle() | Font.BOLD));
+        JPanel tadd4=new JPanel();
+        tadd4.setLayout(new BorderLayout());
+        tadd4.add(labell, BorderLayout.NORTH);
+        tadd3.setLayout(new BorderLayout());
+        tadd3.add(Box.createHorizontalStrut(20),BorderLayout.WEST);
+        tadd3.add(Box.createHorizontalStrut(20),BorderLayout.EAST);
+        tadd3.add(Box.createVerticalStrut(20),BorderLayout.SOUTH);
+        tadd3.add(Box.createVerticalStrut(30),BorderLayout.NORTH);
+        setLayout(new BorderLayout());
+        add(tadd2,BorderLayout.WEST);
+        add(new JSeparator(SwingConstants.VERTICAL), BorderLayout.CENTER);
+        tadd4.add(jtext, BorderLayout.CENTER);
+        tadd3.add(tadd4);
+        add(tadd3,BorderLayout.EAST);
+        pack();
+        setResizable(false);
+        setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+    public void printLine(String text) {
+        jtext.setText(text+System.lineSeparator()+ jtext.getText());
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        repaint();
+    }
+}
